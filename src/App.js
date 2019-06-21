9ca984bbc083f6927a7904806811b3b115fe6b6d
BeardedGeek <muhammad.nadat@gmail.com>
2019-06-21 23:44:06 +0400

    Revert "Loading notification"
    This reverts commit 9a604643b8792aa431480767c7b5c7efb89b01e4.

--- src/App.js
+++ src/App.js
@@ -22,27 +22,48 @@ class App extends Component {
   }
 
   performSearch = (query = 'Art') => {
+<<<<<<< HEAD
     this.setState({
       loading: true
     });
 
+=======
+>>>>>>> parent of 9a60464... Loading notification
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then( response => {
       this.setState({
         query: query,
         images:response.data.photos,
         loading: false
-      });
+      })
     })
     .catch(error => {
       console.log('Error fetching and parsing data', error);
     });
   }
+<<<<<<< HEAD
+=======
+  
+  gallery = ({match}) => {
+    if(match.params.query){
+      this.performSearch(match.params.query);
+    }
+    return(
+      (this.state.loading)
+      ? <p>Loading...</p>
+      : <Gallery 
+          data={this.state.images}
+          query={this.state.query}
+        />
+    )
+  }
+>>>>>>> parent of 9a60464... Loading notification
 
   render() {
     return (
       <Router>
         <div className="container">
+<<<<<<< HEAD
         <Route exact path="/"  render={ () => <SearchForm onSearch={this.performSearch}/> } />
         <Nav />
         {
@@ -56,6 +77,17 @@ class App extends Component {
         </div>
       </Router>
 
+=======
+          <SearchForm onSearch={this.performSearch}/>
+          <Nav onSearch={this.performSearch} />
+          <Switch>
+              <Route exact path="/" component={this.gallery} />
+              <Route exact path="/:query" render={this.gallery} />
+              <Route component={ Route404 } />
+          </Switch>
+        </div>
+      </Router>
+>>>>>>> parent of 9a60464... Loading notification
     );
   }
 }
